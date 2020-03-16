8d7bbb1384ce7575298d35f27fcf13297679fd90
Edd Sansome <edd@murismo.com>
2020-03-16 09:37:08 +0000

    Updates

---
+++ precache-manifest.b25fed0764a9970a8d89175e9a913dcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d1655776ac54ee3b0d1b80e4c6b14b",
+    "url": "/FPL-Rank/index.html"
+  },
+  {
+    "revision": "7c5d571e929a5c04eb8d",
+    "url": "/FPL-Rank/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7c5d571e929a5c04eb8d",
+    "url": "/FPL-Rank/static/js/2.3baad338.chunk.js"
+  },
+  {
+    "revision": "a2e26c9252dea7ee90077b51746a1553",
+    "url": "/FPL-Rank/static/js/2.3baad338.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "637d08a4b8e2fe2f81fc",
+    "url": "/FPL-Rank/static/js/main.000c8b65.chunk.js"
+  },
+  {
+    "revision": "bdd0ac49b51e7360a6f1",
+    "url": "/FPL-Rank/static/js/runtime-main.d7ebabed.js"
+  }
+]);
